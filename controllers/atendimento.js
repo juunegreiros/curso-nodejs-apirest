c26f07361c3d4e7d4161140ec98c2b6bcb21699c
Matheus Hernandes <midia.matheus@gmail.com>
2019-09-11 13:50:46 -0300

    Final aula 3.4

--- controllers/atendimento.js
+++ controllers/atendimento.js
@@ -1,18 +1,15 @@
 const Atendimento = require('../model/atendimentos')
-const { check } = require('express-validator')
 
 module.exports = app => {
   app.get('/atendimentos', (req, res) => {
     console.log('Recebida requisicao atendimento')
-    res.send('Get  Atendimento')
+    Atendimento.lista(res)
   })
 
-  app.post('/atendimentos', [
-    check('cliente').isLength({ min: 5 }).withMessage('nome do cliente deve possuir pelo menos 5 caracteres')
-  ], (req, res) => {
+  app.post('/atendimentos', (req, res) => {
     const atendimento = req.body
     console.log('Atendimento enviado')
     console.log(atendimento)
-    Atendimento.adiciona(req, res, atendimento)
+    Atendimento.adiciona(res, atendimento)
   })
 }
--- model/atendimentos.js
+++ model/atendimentos.js
@@ -1,19 +1,36 @@
 const moment = require('moment')
 const conexao = require('../infraestrutura/conexao')
-const { check, validationResult } = require('express-validator')
 
 class Atendimento {
-  adiciona(req, res, item) {
+  adiciona(res, item) {
     const { cliente, pet, servico, data, status, observacoes } = item
     
     const dataAgendamento = moment(data).format('YYYY-MM-DD hh:mm:ss')
     const dataCriacao = moment().format('YYYY-MM-DD hh:mm:ss')
     
-    const erros = validationResult(req)
+    const dataAgendamentoEhValida = moment(dataAgendamento).isSameOrAfter(dataCriacao)
+    const clienteEhValido = cliente.length >= 5
+
+    const validacao = [
+      { 
+        campo: 'dataAgendamento',
+        valido: dataAgendamentoEhValida,
+        mensagem: 'data deve ser igual ou maior que a data atual'
+      },
+      { 
+        campo: 'cliente',
+        valido: clienteEhValido,
+        mensagem: 'cliente deve conter pelo menos 5 caracteres'
+      }
+    ]
+    
+    const erros = validacao.filter(campo => !campo.valido)
+    
+    console.log(erros)
     const existemErros = !erros.isEmpty
   
     if(existemErros) {
-      res.status(400).json({ erros: erros.array()})
+      res.status(400).json({ erros })
     }
 
     const sql = `INSERT INTO Atendimentos(cliente, pet, servico, status, observacoes, data, dataCriacao) VALUES('${cliente}', '${pet}', '${servico}', '${status}', '${observacoes}', '${dataAgendamento}', '${dataCriacao}')`
@@ -25,6 +42,10 @@ class Atendimento {
         console.log('executou a query!')
       }
     })
+  }
+
+  lista(res) {
+    
   }
 }
 
